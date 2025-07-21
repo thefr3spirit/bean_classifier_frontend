1d5e2ff0341025f60ddad510e5ad2777bf6d12b1
thefr3spirit <beofr3spirit@gmail.com>
2025-07-21 15:01:12 +0300

    the front-end

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageClassifier from "./ImageClassifier";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ImageClassifier />
+    </div>
+  );
+}
+
+export default App;
